2228968ee67beb53082af4a682e9c189e91494b7
Revan Jafar <revanjafar@gmail.com>
2021-07-21 13:12:00 -0700

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// React hooks to use with Firebase
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+// initialize Firebase instance
+firebase.initializeApp({
+  apiKey: "AIzaSyDzq5WEAHSzmE9GzCe4scr0oLbKh2OVo14",
+  authDomain: "ezchat-9b6de.firebaseapp.com",
+  projectId: "ezchat-9b6de",
+  storageBucket: "ezchat-9b6de.appspot.com",
+  messagingSenderId: "736389734062",
+  appId: "1:736389734062:web:a69005cec259e28564973f",
+  measurementId: "G-HVVJ4J7NEW"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
 
       </header>
+
+      
     </div>
   );
 }

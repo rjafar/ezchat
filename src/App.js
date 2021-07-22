5f82f19dffacc8cd6c5bcd5430d360a3d149bb8b
Revan Jafar <revanjafar@gmail.com>
2021-07-21 18:14:36 -0700

    Basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 // React hooks to use with Firebase
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 // initialize Firebase instance
-firebase.initializeApp({
-  apiKey: "AIzaSyDzq5WEAHSzmE9GzCe4scr0oLbKh2OVo14",
-  authDomain: "ezchat-9b6de.firebaseapp.com",
-  projectId: "ezchat-9b6de",
-  storageBucket: "ezchat-9b6de.appspot.com",
-  messagingSenderId: "736389734062",
-  appId: "1:736389734062:web:a69005cec259e28564973f",
-  measurementId: "G-HVVJ4J7NEW"
-})
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDzq5WEAHSzmE9GzCe4scr0oLbKh2OVo14",
+    authDomain: "ezchat-9b6de.firebaseapp.com",
+    projectId: "ezchat-9b6de",
+    storageBucket: "ezchat-9b6de.appspot.com",
+    messagingSenderId: "736389734062",
+    appId: "1:736389734062:web:a69005cec259e28564973f",
+    measurementId: "G-HVVJ4J7NEW"
+  })
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 
 // global firebase variables
 const auth = firebase.auth();
@@ -44,7 +48,6 @@ function App() {
   );
 }
 
-// functional components
 // SignIn allows user to provide Google credentials to authenticate
 function SignIn() {
   const signInGoogle = () => {
@@ -53,21 +56,86 @@ function SignIn() {
   }
 
   return (
-    <button className="signInButton" onClick={signInGoogle}>Sign in with Google</button>
+    <button className="signInButton" onClick={() => signInGoogle()}>Sign in with Google</button>
   )
 }
 
 function SignOut() {
   // check to see if currentUser is valid so we can sign them out
   return auth.currentUser && (
-    <button className="signOutButton" onClick={auth.signOut()}>Sign Out</button>
+    <button className="signOutButton" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
 
-
+// functional component for main chat room
 function Chat () {
+  // reference to end of chat so we don't need to scroll to bottom every time
+  const lastMsgRef = useRef()
 
+  // create a new collection in the firestore database called "messages"
+  const messagesDB = firestore.collection('messages');
+
+  // retrieve 25 of the latest documents in the database document
+  const query = messagesDB.orderBy('createdAt').limit(25);
+
+  // firebase hook that listens to new data coming in and returns array of message objects from DB
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  // keeps track of updated messages being written in form text box
+  const [formInput, setFormInput] = useState('');
+
+  // event handler to submit info from form to the DB
+  const sendMsg = async(e) => {
+    // stop page from refreshing after form submitted
+    e.preventDefault();
+
+    // get user info from current user
+    const { uid, photoURL } = auth.currentUser;
+
+    // write to DB
+    await messagesDB.add({
+      text: formInput,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    // reset formInput state
+    setFormInput('');
+
+    lastMsgRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  // map over array of messages
+  // for each message, create a ChatMessage component with props key and message
+  return (<>
+    <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={lastMsgRef}></span>
+    </main>
+
+    <form onSubmit={sendMsg}>
+      <input value={formInput} onChange={(e) => setFormInput(e.target.value)} placeholder="type here"/>
+      <button type="submit" disabled={!formInput}>Send</button>
+    </form>
+    </>
+  )
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  // check to see who message is from by comparing user id
+  const messageStyle = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+  return (
+    <div className={`${messageStyle}`}>
+      <img src={photoURL || 'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350' } alt=""/>
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
